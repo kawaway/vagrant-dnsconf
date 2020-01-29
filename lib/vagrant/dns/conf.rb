30d93c0f84bc562859a92c608d198f6317979858
kawawa <kawawa.y@gmail.com>
2020-01-29 19:01:32 +0900

    Create template

---
+++ lib/vagrant/dns/conf.rb
@@ -0,0 +1,9 @@
+require "vagrant/dns/conf/version"
+
+module Vagrant
+  module Dns
+    module Conf
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/dns/conf/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Dns
+    module Conf
+      VERSION = "0.1.0"
+    end
+  end
+end
