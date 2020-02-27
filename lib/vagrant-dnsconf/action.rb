dfee7f3d16207eb3377140dae540e035c4b3c7af
kawawa <kawawa.y@gmail.com>
2020-02-28 03:28:49 +0900

    hide verbose log

--- lib/vagrant-dnsconf/action.rb
+++ lib/vagrant-dnsconf/action.rb
@@ -13,10 +13,12 @@ module VagrantPlugins
 	end
 
 	def call(env)
+          logger = Log4r::Logger.new('vagrant::dnsconf')
           @app.call(env) unless @config.is_set?
 	  opts = {replaces: @config.replaces, deletes: @config.deletes}
 
-          @ui.info "setting dns configuration #{opts[:replaces]}, #{opts[:deletes]}"
+	  @ui.info "setting dns configuration"
+          logger.debug "dnsconf #{opts[:replaces]}, #{opts[:deletes]}"
 	  @machine.guest.capability(:set_dnsconf, opts)
 
           @app.call(env)
